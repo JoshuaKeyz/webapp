b248c90a41315c834489f22d49e0a0e7cb55c215
JoshuaKeyz <joshua.oguma@outlook.com>
2017-06-06 22:47:09 +0300

    Added public file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 
 app.use(express.static( __dirname + '/public'));
 app.set('port', (process.env.PORT || 5000));
